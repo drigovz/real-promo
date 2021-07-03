5a0e08aa818dad47b1b2717418a750efcd145f88
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-07-03 17:37:19 -0300

    feat: initial structure project

---
+++ RealPromo/RealPromo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RealPromo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
