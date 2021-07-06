dcd8637c919afd9ab81e166fc16e6b3f684522f4
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-07-05 21:49:40 -0300

    feat: update backend signalr methods

--- RealPromo/RealPromo.Web/Hubs/PromoHub.cs
+++ RealPromo/RealPromo.Web/Hubs/PromoHub.cs
@@ -7,8 +7,9 @@ namespace RealPromo.Web.Hubs
     public class PromoHub : Hub
     {
         public async Task CreatePromotion(Promotion promotion)
-        { 
-
+        {
+            await Clients.Caller.SendAsync("createdSuccessfully");
+            await Clients.Others.SendAsync("receivedPromotion", promotion);
         }
     }
 }
